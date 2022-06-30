e5718df137e1e60d50b71b4e9e24b4c77328f399
Gustavo Santos <gustavo.pantoja@justdigital.com.br>
2022-06-30 02:38:23 -0300

    correçao tela edição

---
+++ database/migrations/2022_06_30_053742_add_img_to_investimentos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('investimentos', function (Blueprint $table) {
+            $table->longText('img')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('investimentos', function (Blueprint $table) {
+            $table->dropColumn('img');
+
+        });
+    }
+};
