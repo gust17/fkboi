e9ba970a8baf2216e9fd43a750d0cd8920075bf4
Gustavo Santos <gustavo.pantoja@justdigital.com.br>
2022-07-05 11:38:03 -0300

    tela Admin

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -30,6 +30,15 @@
 @endif
 
 
+@if ($message = Session::get('status'))
+    <div class="alert alert-info alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+
 @if ($errors->any())
     <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert">×</button>
