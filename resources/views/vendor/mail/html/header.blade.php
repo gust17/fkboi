e889b8144e1f72a64f63ade93bbda2b3b9809384
Gustavo Santos <gustavo.pantoja@justdigital.com.br>
2022-07-04 23:21:27 -0300

    tela Admin

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+<td style="background-color: black" class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+
+<img src="http://fkboiinvest.com.br/wp-content/uploads/2022/05/LOGO-COMPACTADA.jpg" class="logo" alt="Laravel Logo">
+
+</a>
+</td>
+</tr>
